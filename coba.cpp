2cda55d9ef872490153879877d6de8da8f1880b0
elwinspranata <elwinpranata02@gmail.com>
2023-03-08 16:07:32 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "selamat datang";
+    return 0;
+
+}
---
+++ tugas1.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;
+
+int main (){//begin
+
+//nuumeric luas, P, L 
+double Luas, P, L;
+//Display, "masukan panjangnya: "
+cout << "masukan panjangnya : ";
+//Accept P
+cin >> P;
+//Display, "masukan Lebarnya : "
+cout << "masukan Lebarnya : ";
+//Accept L
+cin >> L;
+//compute Luas = P*L 
+Luas = P*L;
+//Display, "Luasnya : " + Luas 
+cout << "luasnya : " << Luas; 
+}//end"}
